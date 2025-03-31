04079234955f07610223508b63d462facefc5472
pediland <pediland.com@gmail.com>
2025-03-31 18:57:22 +0200

    Footer Layout

--- parts/footer.php
+++ parts/footer.php
@@ -1,7 +1,64 @@
-<footer class="bg-gray-100 py-3 md:py-6">
-    <div class="container max-w-7xl mx-auto px-3 md:px-0">
-        <div class="_copy-right text-center">
-            <p class="text-gray-500 textarea-md">تمامی حقوق ناشی از این وب سایت برای <?php bloginfo('name'); ?> محفوظ است. <?php echo date('Y'); ?>&copy;</p>
+<footer>
+    <div class="_footer pb-8 md:pb-18 pt-20 md:pt-28 bg-gradient-to-t from-gray-200 to-white">
+        <div class="container max-w-7xl mx-auto px-6 md:px-0">
+            <div class="_wrapper flex flex-col md:flex-row items-center gap-12">
+                <div class="w-full md:w-2/5">
+                    <div class="_footer-content flex flex-col items-start gap-4">
+                        <p class="text-gray-500 text-[15px]">بفروش ابزاری هوشمند در حوزه فروش و ارسال سفارش‌های آنلاین است و به فروشگاه‌های اینترنتی و فروشندگان در شبکه‌های اجتماعی، امکان مدیریت و نظارت بر فرآیند فروش و ارسال سفارش‌ها را می‌دهد.</p>
+                        <div class="_socials flex flex-col gap-3">
+                            <h4 class="font-semibold">بفروش را دنبال کنید:</h4>
+                            <div class="_items flex items-center gap-3 md:gap-6">
+                                <div class="_item">
+                                    <a href="https://www.instagram.com/befroosh.app" target="_blank" rel="noopener noreferrer" data-tip="اینستاگرام" class="flex items-center text-gray-400 hover:text-fuchsia-700 duration-300 tooltip"><i class="ph ph-instagram-logo text-3xl"></i></a>
+                                </div>
+                                <div class="_item">
+                                    <a href="https://aparat.com/befroosh" target="_blank" rel="noopener noreferrer" data-tip="آپارات" class="flex items-center text-gray-400 hover:text-pink-600 duration-300 tooltip"><i class="ph ph-film-reel text-3xl"></i></a>
+                                </div>
+                                <div class="_item">
+                                    <a href="https://www.linkedin.com/company/befroosh" target="_blank" rel="noopener noreferrer" data-tip="لینکدین" class="flex items-center text-gray-400 hover:text-blue-600 duration-300 tooltip"><i class="ph ph-linkedin-logo text-3xl"></i></a>
+                                </div>
+                                <div class="_item">
+                                    <a href="https://t.me/befroosh_app" target="_blank" rel="noopener noreferrer" data-tip="کانال تلگرام" class="flex items-center text-gray-400 hover:text-blue-500 duration-300 tooltip"><i class="ph ph-telegram-logo text-3xl"></i></a>
+                                </div>
+                                <div class="_item">
+                                    <a href="https://x.com/befroosh" target="_blank" rel="noopener noreferrer" data-tip="ایکس" class="flex items-center text-gray-400 hover:text-gray-900 duration-300 tooltip"><i class="ph ph-x-logo text-3xl"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full md:w-3/5">
+                    <div class="_footer-tools grid grid-cols-2 md:grid-cols-3 gap-10 md:gap-12">
+                        <div class="_item flex items-center justify-center col-span-2 md:col-span-1">
+                            <img class="w-28 rounded-lg" src="<?php echo get_template_directory_uri(); ?>/assets/images/enamad.png" alt="e-Namad Logo">
+                        </div>
+                        <div class="_item col-span-1">
+                            <ul class="text-[15px] leading-7 text-gray-500 [&>li>a]:hover:text-blue-900 duration-300">
+                                <li><a href="/blog">بلاگ بفروش</a></li>
+                                <li><a href="/faq">سوالات متداول</a></li>
+                                <li><a href="/pricvacy">حریم خصوصی کاربران</a></li>
+                                <li><a href="/terms">قوانین و مقررات</a></li>
+                            </ul>
+                        </div>
+                        <div class="_item col-span-1">
+                            <ul class="text-[15px] leading-7 text-gray-500 [&>li>a]:hover:text-blue-900 duration-300">
+                                <li><a href="/about">درباره بفروش</a></li>
+                                <li><a href="/features">امکانات</a></li>
+                                <li><a href="/pricing">تعرفه‌ها</a></li>
+                                <li><a href="/contact">اطلاعات تماس</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="_copy-right bg-gray-200 py-4 md:py-6">
+        <div class="container max-w-7xl mx-auto px-3 md:px-0">
+            <div class="_wrapper">
+                <p class="text-gray-400 text-sm text-center">© تمامی حقوق ناشی از این وب سایت برای <span class="text-gray-600"><?php bloginfo('name'); ?></span> محفوظ است.</p>
+            </div>
         </div>
     </div>
 </footer>
